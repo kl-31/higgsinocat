0e9ad1be1ac29fdff5f76c1d8195232255f61a87
kaicheng liang <kliang933@gmail.com>
2019-05-11 10:06:02 +0800

    change queries for physics

--- scrape_from_pubmed_arxiv.py
+++ scrape_from_pubmed_arxiv.py
@@ -23,27 +23,27 @@ import feedparser
 from unidecode import unidecode
 
 
-
-def search(query,retmax):
-	Entrez.email = 'your.email@example.com'
-	handle = Entrez.esearch(db='pubmed', 
-							sort='relevant', 
-							retmax=str(retmax),
-							retstart = '10',
-							retmode='xml', 
-							#reldate = '3650',
-							term=query)
-	results = Entrez.read(handle)
-	return results
-
-def fetch_details(id_list):
-	ids = ','.join(id_list)
-	Entrez.email = 'your.email@example.com'
-	handle = Entrez.efetch(db='pubmed',
-                           retmode='xml',
-                           id=ids)
-	results = Entrez.read(handle)
-	return results
+#
+#def search(query,retmax):
+#	Entrez.email = 'your.email@example.com'
+#	handle = Entrez.esearch(db='pubmed', 
+#							sort='relevant', 
+#							retmax=str(retmax),
+#							retstart = '10',
+#							retmode='xml', 
+#							#reldate = '3650',
+#							term=query)
+#	results = Entrez.read(handle)
+#	return results
+#
+#def fetch_details(id_list):
+#	ids = ','.join(id_list)
+#	Entrez.email = 'your.email@example.com'
+#	handle = Entrez.efetch(db='pubmed',
+#                           retmode='xml',
+#                           id=ids)
+#	results = Entrez.read(handle)
+#	return results
 
 if __name__ == '__main__':
 	chunk_size = 50
@@ -73,26 +73,26 @@ if __name__ == '__main__':
 #				print('%d titles saved in %s.' % (num,query))
 	
 	# ARXIV QUERY
-		num=0
-		for chunk_i in range(0, 30000, chunk_size):
-			feed = feedparser.parse('http://export.arxiv.org/api/query?search_query=cat:astro-ph&start=%d&max_results=%d' % (chunk_i,chunk_size))
-			
-			for i in range(len(feed.entries)):
-				entry = feed.entries[i]
-				title = (entry.title).replace('\n', "")
-				file_writer.writerow([unidecode(title), str(0)])
-				num = num+1
-		print('%d titles saved from Arxiv astro-ph.' % num)	
-		num=0
-		for chunk_i in range(0, 30000, chunk_size):
-			feed = feedparser.parse('http://export.arxiv.org/api/query?search_query=all:dark+AND+all:matter&start=%d&max_results=%d' % (chunk_i,chunk_size))
-			
-			for i in range(len(feed.entries)):
-				entry = feed.entries[i]
-				title = (entry.title).replace('\n', "")
-				file_writer.writerow([unidecode(title), str(1)])
-				num = num+1
-		print('%d titles saved from Arxiv search of \'dark matter\'.' % num)		
+		groups = [['all:%22dark+energy%22', 'cat:astro-ph+ANDNOT+all:%22dark+matter%22','cat:hep-ph+ANDNOT+all:%22dark+matter%22'],
+				   ['all:%22primordial+black+hole%22', 'all:%22dark+sector%22', 'all:%22dark+matter%22']]
+
+		for group in groups:
+			for query in group:
+				num=0
+				if query[0:3] == 'all' and ('dark+matter' not in query):
+					retmax = 5000
+				else:
+					retmax = 30000
+				for chunk_i in range(0, retmax, chunk_size):
+					feed = feedparser.parse('http://export.arxiv.org/api/query?search_query=%s&start=%d&max_results=%d' % (query,chunk_i,chunk_size))
+					
+					for i in range(len(feed.entries)):
+						entry = feed.entries[i]
+						title = (entry.title).replace('\n', "")
+						file_writer.writerow([unidecode(title), str(groups.index(group))])
+						num = num+1
+				print('%d titles saved from Arxiv %s.' % (num, query))
+
 	
 	
 	end = time.time()
