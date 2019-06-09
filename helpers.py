ff1475ce1e7d3db7512c99a119df2541cee3db93
kaicheng liang <kliang933@gmail.com>
2019-06-09 18:17:49 +0800

    use more recent (100) tweets for wkend rt

--- helpers.py
+++ helpers.py
@@ -262,7 +262,7 @@ def retweet_old(number):
 	api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True,retry_count=10, retry_delay=5, retry_errors=set([503]))
 	tweets = []
 	# retweeting tweets
-	for page in tweepy.Cursor(api.user_timeline, count = 200).pages():
+	for page in tweepy.Cursor(api.user_timeline, count = 100).pages():
 			tweets.extend(page)
 			sleep(5)	
 	
