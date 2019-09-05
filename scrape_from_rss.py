304990dc7f979c22f4e32d6ade818c14b83f1a57
Kaicheng Liang <kliang933@gmail.com>
2019-09-05 08:40:55 +0800

    back to 30 min interval

--- scrape_from_rss.py
+++ scrape_from_rss.py
@@ -18,7 +18,7 @@ else:
 					}
 
 time = 6 * 60 # 6 hours
-interval = 1 # minutes
+interval = 30 # minutes
 written = np.zeros(len(feed_info.keys()),dtype=np.int)
 posted = np.zeros(len(feed_info.keys()),dtype=np.int)
 attempts = 0
