1bd582e57bdba8cea2a24934eae57f3e919c9c20
Kaicheng Liang <kliang933@gmail.com>
2019-09-09 22:43:50 +0800

    confirmed working. set correct times

--- clock.py
+++ clock.py
@@ -12,22 +12,22 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', hour=1)
 def scheduled_job1():
-	print('This job is run every weekday at 1am UTC.')
+	print('This job is run every weekday at 1am UTC (9am SGT).')
 	subprocess.run(['python', 'scrape_from_rss.py'])
 
 @sched.scheduled_job('cron', hour=7)
 def scheduled_job2():
-	print('This job is run every weekday at 7am UTC.')
+	print('This job is run every weekday at 7am UTC (3pm SGT).')
 	subprocess.run(['python', 'scrape_from_rss.py'])
 	
-@sched.scheduled_job('cron', hour=14, minute=20)
+@sched.scheduled_job('cron', hour=13)
 def scheduled_job3():
-	print('This job is run every weekday at 1pm UTC.')
+	print('This job is run every weekday at 1pm UTC (9pm SGT).')
 	subprocess.run(['python', 'scrape_from_rss.py'])
 	
 @sched.scheduled_job('cron', hour=19)
 def scheduled_job4():
-	print('This job is run every weekday at 7pm UTC.')
+	print('This job is run every weekday at 7pm UTC (3am SGT).')
 	subprocess.run(['python', 'scrape_from_rss.py'])
 
 sched.start()
